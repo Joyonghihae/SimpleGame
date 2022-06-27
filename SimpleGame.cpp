30e375ce25a4dc09da08354e70b47c15a2beb27b
Joyonghihae <rladms010@gmail.com>
2022-06-27 17:52:44 -0400

    make the character

---
+++ SimpleGame.cpp
@@ -0,0 +1,38 @@
+﻿// git
+
+
+#include<stdio.h>
+
+
+int main() {
+
+	bool repeat = true;
+
+	for (;;) {
+
+		printf("     _    _ \n");
+		printf("    | |  | | \n");
+		printf("    | |  | | \n");
+		printf("   /        \\ \n");
+		printf("   | o    o |\n");
+		printf("    \\__ -__/ \n");
+		printf("    /      \\ \n");
+		printf("  _/ |    | \\_ \n");
+		printf("     |    | \n");
+		printf("    *| -- | \n");
+
+		if (repeat == true) {
+
+			printf("     |_|-- \n");
+			repeat = false;
+
+		}
+		else {
+			printf("     --|_| \n");
+			repeat = true;
+		}
+	}
+
+	
+	return 0;
+}
